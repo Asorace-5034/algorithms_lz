b56f24b4f6b3f973f1b4b4fa1705df8312f2cff2
Asorace-5034 <tafytoo@gmail.com>
2025-10-31 17:02:52 +0300

    добавил

--- sort.py
+++ sort.py
@@ -5,8 +5,8 @@ nums_1 = [] #создаем список 1
 quantity_1 = 0 # количество операций
 quantity_2 = 0
 
-for i in range(10):
-    nums_1.append(rd.randint(1,100)) # создаем массив чисел по условию
+for i in range(100):
+    nums_1.append(rd.randint(1,1000000)) # создаем массив чисел по условию
 
 nums_2 = nums_1.copy()
 
@@ -25,19 +25,23 @@ def choose_sort (nums):
         nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
         global quantity_1
         quantity_1 += 1
+    return nums
         
         
 # сортировка методом пузырька в убывание
 
 def choose_buble (nums):
-    for i in range(len(nums_2)):
-        print(nums)
-        for j in range(len(nums_2) - 1 - i):
-            if nums[j] < nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
-            
-        global quantity_2
-        quantity_2 +=1
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+                global quantity_2
+                quantity_2 += 1
+                print(nums)
+    return nums
 
 choose_sort(nums_1) #вызываем функцию для сортировки выборкой и печататем готовый список
 print(f"Количество операций методом выборки: {quantity_1}")
